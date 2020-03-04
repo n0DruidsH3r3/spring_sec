e639075fb129f8ed999b72694327356decb4e56c
n0druidsh3r3 <n0druidsh3r3@gmail.com>
2020-03-04 14:55:58 +0100

    use of user detail service instead of authManagerB

--- src/main/java/totorial/security/security/AppSecurityConfig.java
+++ src/main/java/totorial/security/security/AppSecurityConfig.java
@@ -1,30 +1,39 @@
 package totorial.security.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth, AppSecurityConfigProperties conf) throws Exception {
-        var inMemAuth = auth.inMemoryAuthentication();
-        for (var appUser : conf.getUsers()) {
-            inMemAuth.withUser(appUser.getUsername())
-                    .password(appUser.getPassword())
-                    .roles(appUser.getRole());
+    private AppSecurityConfigProperties conf;
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        for (var user : conf.getUsers()) {
+            userDetailsManager.createUser(User.withUsername(user.getUsername())
+                    .password(user.getPassword())
+                    .roles(user.getRole())
+                    .build());
         }
+        return userDetailsManager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
-                .and().httpBasic().and().userDetailsService()
+                .and().httpBasic();
     }
 }
